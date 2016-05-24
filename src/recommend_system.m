6016ef547f1f5fd9f5d878d1b4a3ea22e9923a4f
bin liu <bin liu>
2016-05-24 23:26:39 +0800

    初始框架

--- src/recommend_system.m
+++ src/recommend_system.m
@@ -1,7 +1,14 @@
+%% Preprocess
+% Add the current folder & subfolders to the top of the search path of MATLAB
+pwdstr = pwd;
+p = genpath(pwdstr);
+addpath(p, '-begin');
+savepath;
 % clc; clear; close;
 
-%% load data
+%% load origin data
 % In this part, data was load and save with name test.mat
+fprintf('load origin data\n');
 origin_data = 'origin_data.mat';
 if ~exist(origin_data, 'file')
     data_path = '../data/';
@@ -17,18 +24,19 @@ end
 %% process the data
 % In this part, data was processed and num_user, num_article, ...
 % train, test will be calculate.
+fprintf('processe the data\n');
 processed_data = 'processed_data.mat';
 if ~exist(processed_data, 'file')
     num = max(train_origin);
-    num_user = num(1);
-    num_article = num(2);
-    train_processed = zeros(num_user, num_article);
+    num_users = num(1);
+    num_articles = num(2);
+    train_processed = zeros(num_users, num_articles);
     for i = 1 : size(train_origin, 1)
         x = train_origin(i, 1);
         y = train_origin(i, 2);
         train_processed(x, y) = 1;
     end
-    test_processed = zeros(num_user, num_article);
+    test_processed = zeros(num_users, num_articles);
     for i = 1 : size(test_origin, 1)
         x = test_origin(i, 1);
         y = test_origin(i, 2);
@@ -42,9 +50,22 @@ end
 %% recommend article for users
 % In this part, recommend algorithm was called and save with ...
 % name r_usercf, r_itemcf, r_content
+fprintf('recommend\n');
 k = 10;
-r_usercf  = UserCF_recommend     (train, num_user, num_article, k);
-r_item_cf = ItemCF_recommend     (train, num_user, num_article, k);
-r_content = ItemContent_recommend(train, num_user, num_article, k);
+r_usercf  = UserCF_recommend ...   
+    (train_processed, num_users, num_articles, k);
+r_itemcf = ItemCF_recommend ...
+    (train_processed, num_users, num_articles, k);
+r_content = ItemContent_recommend...
+    (train_processed, num_users, num_articles, k);
+% TODO: sort the recommendation by the weight.
+recommendation = ...
+    [r_usercf(:, :, 1); r_itemcf(:, :, 1); r_content(:, :, 1)];
+
+%% recommend test
+% In this part, we will test the precision and recall of our system.
+[recall, precision] = recommendation_test(recommendation, test_processed);
+fprintf('recall = %f\n precision = %f\n', recall, precision);
+
 
 
