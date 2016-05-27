112424c6c7e4b7cb003f93513bb30066f9c46c31
Shuang li <lsa14@mails.tsinghua.edu.cn>
2016-05-27 20:16:57 +0800

    testRecommend

--- src/test/recommendation_test.m
+++ src/test/recommendation_test.m
@@ -1,3 +1,15 @@
-function [precision, recall] = recommendation_test(recommendation, test)
-
+function [precision, recall] = recommendation_test(recommendation, test,num_user,k)
+    precision = zeros(num_user,1);
+    recall = zeros(num_user,1);
+    for i = 1:num_user
+        test_total = sum(test(i,:)==1);
+        target = 0;
+        for j = 1:k
+            if test(i,recommendation(i,j,2)) == 1
+                target = target + 1;
+            end
+        end
+        precision(i) = target/k;
+        recall(i) = target/test_total;
+    end
 end
